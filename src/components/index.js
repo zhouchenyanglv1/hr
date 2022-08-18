d340b406efa2febc8268855a02f11a33c232a6d5
zhouchenyang <278938294@qq.com>
2022-08-18 21:02:35 +0800

    import页面

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 import pagetools from '@/components/Pagrtools'
+import uploadExcel from '@/components/uploadExcel'
 export default {
   install(vue) {
     vue.component('pagetools', pagetools)
+    vue.component('uploadExcel', uploadExcel)
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -61,7 +61,16 @@ export const constantRoutes = [
       meta: { title: '首页', icon: 'dashboard' }
     }]
   },
+  {
+    path: '/import',
+    component: Layout,
+    hidden: true,
+    children: [{
+      path: '',
+      component: () => import('@/views/import/index')
+    }]
 
+  },
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
 ]
